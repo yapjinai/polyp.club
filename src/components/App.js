6c80a3fb507a7f7ed36574654935c3c2eda01005
yapjinai <jinai.jyap@gmail.com>
2019-04-02 20:43:52 -0400

    Update main page

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-// import './App.css';
+import '../css/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>POLYP!</h1>
+        Coming soon :o)
       </div>
     );
   }

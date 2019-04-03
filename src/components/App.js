69c1ae9deeda2bd2cf4fc70325092cdc823b0d3b
yapjinai <jinai.jyap@gmail.com>
2019-04-02 21:33:06 -0400

    Added image + link to whoracle

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import best from '../img/best-or-what.png';
 import '../css/App.css';
+
+const whoracleURL = 'http://whoracle.herokuapp.com'
 
 class App extends Component {
   render() {
@@ -7,6 +10,9 @@ class App extends Component {
       <div className="App">
         <h1>POLYP!</h1>
         Coming soon :o)
+        <a href={whoracleURL}>
+          <img src={best} alt='best' />
+        </a>
       </div>
     );
   }

de14420bcae8ad6c97f167db5965e77cc046f3da
yapjinai <jinai.jyap@gmail.com>
2019-04-28 11:53:13 -0400

    Delete old API url

--- src/pages/whoracle/Result.js
+++ src/pages/whoracle/Result.js
@@ -4,7 +4,6 @@ import questionToQueries from "../../utils/questionToQueries"
 import "../../css/Result.css";
 
 const API_URL = "https://whoracle-api.herokuapp.com"
-// const API_URL = "http://localhost:3000"
 
 class Result extends Component {
   constructor() {
